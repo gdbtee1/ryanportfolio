dec916fe533d2edb9065b0b2c04337ff2f65fcca
Techuvo <techuvo.dev@gmail.com>
2026-07-22 11:27:29 -0400

    Configure GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
-import App from "./App";
+
+import App from "./App.jsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>,
+createRoot(document.getElementById("root")).render(
+  <HashRouter>
+    <App />
+  </HashRouter>,
 );
